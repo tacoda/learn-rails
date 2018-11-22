65b8dca6b8d4a098754bb48567fea999015a7c4f
Ian Johnson <tacoda@pm.me>
2018-11-21 20:46:10 -0500

    Add create and index methods

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,4 +2,24 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+
+  # Parameters
+  def index
+    if params[:status] == "activated"
+      @clients = Client.activated
+    else
+      @clients = Client.inactivated
+    end
+  end
+
+  def create
+    @client = Client.new(params[:client])
+    if @client.save
+      redirect_to @client
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
+  end
 end
